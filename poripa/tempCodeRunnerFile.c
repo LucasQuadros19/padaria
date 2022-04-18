0ae6d7470d63d506d86ec8814bcf484ca7d39745
dogogh <quadros1919@gmail.com>
2022-04-17 23:40:28 -0300

    save

---
+++ poripa/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+for(i=1;i<=item;i++){
+            printf("\titems=%d\tnome=%s \tpreco unitario=R$%0.2f\tquantidade=%d\tsub valor=R$%0.2f\n ",i,nome[nome3[i]],uni[i],quant2[i],svalor[i]);
+    }
+        printf("\n\t\t\t\t\tvalor total=R$%0.2f",pre2);
